2cf377c695f1deeec085deef1bee12d5487b4d42
bicarus-dev <202771338+bicarus-dev@users.noreply.github.com>
2025-03-15 22:15:02 -0700

    Fix clipboard copy string function

--- src/spice2x/misc/clipboard.h
+++ src/spice2x/misc/clipboard.h
@@ -4,6 +4,6 @@
 
 namespace clipboard {
     void copy_image(const std::filesystem::path path);
-    void copy_text(const std::string str);
+    void copy_text(const std::string& str);
     const std::string paste_text();
 }
