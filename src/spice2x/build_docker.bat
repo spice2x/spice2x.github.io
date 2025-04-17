230687ac241be756bd42df43ee9c8554f97ccae5
ASleepyCat <dyeom340@gmail.com>
2025-04-17 20:08:24 +1000

    Remove --no-cache flag in build scripts

--- src/spice2x/build_docker.bat
+++ src/spice2x/build_docker.bat
@@ -1,5 +1,5 @@
 docker build --pull external/docker -t spicetools/deps
-docker build --build-context gitroot=%cd%/../../.git . -t spicetools/spice --no-cache
+docker build --build-context gitroot=%cd%/../../.git . -t spicetools/spice
 docker run --rm -it -v %cd%/dist:/src/src/spice2x/dist -v %cd%/bin:/src/src/spice2x/bin -v %cd%/.ccache:/ccache spicetools/spice
 @REM to generate PDBs, set DEBUG to 1 in build_all.sh, place cv2pdb in external\cv2pdb, and run below
 @REM external\cv2pdb\cv2pdb.exe bin\spice2x\spicecfg.exe bin\spice2x\spicecfg-pdb.exe bin\spice2x\spicecfg-pdb.pdb
--- src/spice2x/build_docker.sh
+++ src/spice2x/build_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build --pull $PWD/external/docker -t spicetools/deps --platform linux/x86_64
-docker build --build-context gitroot=$PWD/../../.git . -t spicetools/spice:latest --no-cache
+docker build --build-context gitroot=$PWD/../../.git . -t spicetools/spice:latest
 docker run --rm -v $PWD/dist:/src/src/spice2x/dist -v $PWD/bin:/src/src/spice2x/bin -v $PWD/.ccache:/ccache spicetools/spice
