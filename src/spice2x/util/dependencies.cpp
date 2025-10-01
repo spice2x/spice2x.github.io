70c47dc0c5283109b27da093f3bb9cec2b9044c2
llm96 <llm96@fervidex.net>
2025-10-01 02:31:59 +0100

    Style consistency fixups in dependencies.cpp

--- src/spice2x/util/dependencies.cpp
+++ src/spice2x/util/dependencies.cpp
@@ -47,37 +47,43 @@ namespace {
         auto result = std::vector<std::filesystem::path> {};
         auto const file = CreateFileW(path.c_str(), GENERIC_READ, FILE_SHARE_READ, nullptr, OPEN_EXISTING, 0, nullptr);
 
-        if (file == INVALID_HANDLE_VALUE)
+        if (file == INVALID_HANDLE_VALUE) {
             return result;
+        }
 
         auto const file_ = scope_guard { [file] { CloseHandle(file); } };
         auto const mapping = CreateFileMapping(file, nullptr, PAGE_READONLY, 0, 0, nullptr);
 
-        if (!mapping)
+        if (!mapping) {
             return result;
+        }
 
         auto const mapping_ = scope_guard { [mapping] { CloseHandle(mapping); } };
         auto const view = MapViewOfFile(mapping, FILE_MAP_READ, 0, 0, 0);
 
-        if (!view)
+        if (!view) {
             return result;
+        }
 
         auto const view_ = scope_guard { [view] { UnmapViewOfFile(view); } };
         auto const dos = static_cast<PIMAGE_DOS_HEADER>(view);
 
-        if (dos->e_magic != IMAGE_DOS_SIGNATURE)
+        if (dos->e_magic != IMAGE_DOS_SIGNATURE) {
             return result;
+        }
 
         auto const base = static_cast<std::uint8_t*>(view);
         auto const nt = reinterpret_cast<PIMAGE_NT_HEADERS>(base + dos->e_lfanew);
 
-        if (nt->Signature != IMAGE_NT_SIGNATURE)
+        if (nt->Signature != IMAGE_NT_SIGNATURE) {
             return result;
+        }
 
         auto const imports = nt->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT];
 
-        if (imports.VirtualAddress == 0)
+        if (imports.VirtualAddress == 0) {
             return result;
+        }
 
         auto desc = reinterpret_cast<PIMAGE_IMPORT_DESCRIPTOR>(base + libutils::rva2offset(nt, imports.VirtualAddress));
 
@@ -87,8 +93,9 @@ namespace {
 
             // try to use library in the same directory if one exists
             // otherwise, assume system library and use filename only
-            if (!std::filesystem::exists(entry_path))
+            if (!std::filesystem::exists(entry_path)) {
                 entry_path = filename;
+            }
 
             result.emplace_back(entry_path);
             desc++;
@@ -111,8 +118,9 @@ namespace dependencies {
 
         log_misc("dependencies", "{}{}", prefix + (prefix.empty() ? "": "|-- "), filename);
 
-        if (failed.contains(path))
+        if (failed.contains(path)) {
             return false;
+        }
 
         failed.insert(path);
 
@@ -120,15 +128,17 @@ namespace dependencies {
         auto const next_prefix = prefix + (prefix.empty() ? "  ": "| ");
 
         if (!dependencies.empty()) {
-            for (auto const& item: dependencies)
+            for (auto const& item : dependencies) {
                 walk(item, next_prefix);
+            }
 
             return false;
         }
 
-        for (auto const& [dll, name, hint]: hints) {
-            if (_stricmp(dll.c_str(), filename.c_str()) != 0)
+        for (auto const& [dll, name, hint] : hints) {
+            if (_stricmp(dll.c_str(), filename.c_str()) != 0) {
                 continue;
+            }
 
             log_warning("dependencies", "{}|-- [!] {}", next_prefix, name.c_str());
             log_warning("dependencies", "{}|--     {}", next_prefix, hint.c_str());
