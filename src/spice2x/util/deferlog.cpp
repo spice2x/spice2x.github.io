b330c80490d780df545c3df077419d883469fdd8
bicarus-dev <202771338+bicarus-dev@users.noreply.github.com>
2025-10-04 07:35:38 -0700

    fix deadlock again

--- src/spice2x/util/deferlog.cpp
+++ src/spice2x/util/deferlog.cpp
@@ -8,7 +8,6 @@ namespace deferredlogs {
 
     const std::initializer_list<std::string> SUPERSTEP_SOUND_ERROR_MESSAGE = {
         "audio initialization error was previously detected during boot!",
-        "    (W:SuperstepSound: Audio device is not available!!!)",
         "    this crash is most likely related to audio init failure",
         "      * check if the default audio device has changed",
         "      * fix your audio device settings (e.g., sample rate)",
@@ -17,14 +16,8 @@ namespace deferredlogs {
 
     std::mutex deferred_errors_mutex;
     std::vector<std::vector<std::string>> deferred_errors;
-    std::atomic<bool> too_late;
 
     void defer_error_messages(std::initializer_list<std::string> messages) {
-        // don't try to acquire lock if already dumping to logger
-        if (too_late) {
-            return;
-        }
-
         std::lock_guard<std::mutex> lock(deferred_errors_mutex);
         deferred_errors.emplace_back(messages);
     }
@@ -32,15 +25,24 @@ namespace deferredlogs {
     void dump_to_logger() {
         static std::once_flag printed;
         std::call_once(printed, []() {
-            too_late = true;
-            if (deferred_errors.empty()) {
-                return;
+
+            // move to a local vector under lock first
+            // this is to avoid holding a lock while emitting to the logger, which may deadlock
+            // due to recursive calls (e.g., via log hooks used for failure detection which then
+            // again calls into defer_error_messages), and std::mutex cannot be acquired recursively
+            std::vector<std::vector<std::string>> errors;
+            {
+                std::lock_guard<std::mutex> lock(deferred_errors_mutex);
+                if (deferred_errors.empty()) {
+                    return;
+                }
+                errors = std::move(deferred_errors);
             }
 
             log_warning("troubleshooter", "/------------------------ spice2x auto-troubleshooter ------------------------\\");
             log_warning("troubleshooter", "");
 
-            for (auto messages : deferred_errors) {
+            for (auto messages : errors) {
                 for (auto message : messages) {
                     log_warning("troubleshooter", "  {}", message);
                 }
