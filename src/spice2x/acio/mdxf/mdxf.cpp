f05f9bb23bb580dd38e68b1cab8a499f64932a16
cchike <cchike4@gmail.com>
2025-12-17 23:09:15 -0800

    Comment update

--- src/spice2x/acio/mdxf/mdxf.cpp
+++ src/spice2x/acio/mdxf/mdxf.cpp
@@ -59,9 +59,10 @@ typedef enum {
 } MDXFBufferFillMode;
 
  /* Decides which method to use for populating ring buffer entries for "padding". Could possibly be made configurable through spicecfg.
-    THREAD_MODE: Spins a thread running at THREAD_REFRESH_RATE_HZ which periodically fills the ring buffer with auxiliary entries
-    BACKFILL_MODE: On every update cycle, fill the ring buffer with entries for the last known state BACKFILL_INTERVAL_MS apart from each other...
-    ...from the time of the last entry to the current time before adding the entry for the current state */
+    THREAD_MODE: Spins a thread running at THREAD_REFRESH_RATE_HZ which periodically fills the ring buffer with auxiliary entries. Falls back on BACKFILL_MODE
+        if the game's refresh rate is fast enough to forego starting the thread.
+    BACKFILL_MODE: On every update cycle, fill the ring buffer with entries for the last known state BACKFILL_INTERVAL_MS apart from each other 
+        from the time of the last entry to the current time before adding the entry for the current state. */
 static const MDXFBufferFillMode BUFFER_FILL_MODE = THREAD_MODE;
 
 typedef enum {
