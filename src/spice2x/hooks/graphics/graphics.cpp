1b92e1b126d3767ed3928ab25cb8350c3e50c78d
bicarus-dev <202771338+bicarus-dev@users.noreply.github.com>
2026-01-02 05:15:25 -0800

    comments

--- src/spice2x/hooks/graphics/graphics.cpp
+++ src/spice2x/hooks/graphics/graphics.cpp
@@ -670,7 +670,9 @@ static BOOL WINAPI SetWindowPos_hook(HWND hWnd, HWND hWndInsertAfter,
         return TRUE;
     }
 
+    // prevent gitadora arena model from shifting windows around if the user has preferences
     if (GRAPHICS_WINDOWED && games::gitadora::is_arena_model() &&
+        GRAPHICS_WINDOW_MAIN.has_value() && hWnd == GRAPHICS_WINDOW_MAIN.value() &&
         cfg::SCREENRESIZE->enable_window_resize) {
         return TRUE;
     }
