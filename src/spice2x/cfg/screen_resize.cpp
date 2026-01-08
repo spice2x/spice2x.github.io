spice2x/spice2x.github.io

commit be2dab9a1a9f6c21e863f2d6f5c8c895f97924f7

Image duplication for screen resize (#401)
## Link to GitHub Issue, if one exists
#399 

## Description of change
Add image duplication option to screen resize, allowing overflow images
to wrap around the other side.

## Testing
Tested with DDR, IIDX (windowed and full screen TDJ), SDVX (orientation
swap)

src/spice2x/cfg/screen_resize.cpp
@@ -87,6 +87,10 @@ namespace cfg {
             load_float_value(doc, root + prefix + "scale_x", scene.scale_x);
             load_float_value(doc, root + prefix + "scale_y", scene.scale_y);
             load_bool_value(doc, root + prefix + "keep_aspect_ratio", scene.keep_aspect_ratio);
+
+            int duplicate = 0;
+            load_int_value(doc, root + prefix + "duplicate", duplicate);
+            scene.duplicate = static_cast<cfg::ScreenDuplicateMode>(duplicate);
         }
 
         // windowed settings are always under game settings
@@ -201,6 +205,7 @@ namespace cfg {
             rapidjson::Pointer(root + prefix + "scale_x").Set(doc, scene.scale_x);
             rapidjson::Pointer(root + prefix + "scale_y").Set(doc, scene.scale_y);
             rapidjson::Pointer(root + prefix + "keep_aspect_ratio").Set(doc, scene.keep_aspect_ratio);
+            rapidjson::Pointer(root + prefix + "duplicate").Set(doc, scene.duplicate);
         }
 
         // windowed mode settings

src/spice2x/cfg/screen_resize.h
@@ -14,12 +14,19 @@ namespace cfg {
         ResizableFrame = 2
     };
 
+    enum ScreenDuplicateMode {
+        None = 0,
+        CopyLeft = 1,
+        CopyRight = 2,
+    };
+
     struct fullscreen_setting {
         int offset_x = 0;
         int offset_y = 0;
         float scale_x = 1.0;
         float scale_y = 1.0;
         bool keep_aspect_ratio = true;
+        ScreenDuplicateMode duplicate = ScreenDuplicateMode::None;
     };
 
     extern std::optional<std::string> SCREEN_RESIZE_CFG_PATH_OVERRIDE;

src/spice2x/hooks/graphics/backends/d3d9/d3d9_device.cpp
@@ -733,6 +733,16 @@ void SurfaceHook(IDirect3DDevice9 *pReal) {
     const int rectTop = param.BackBufferHeight;
     const int w = param.BackBufferWidth;
     const int h = param.BackBufferHeight;
+
+    if (cfg::SCREENRESIZE->enable_screen_resize) {
+        RECT rect {
+            0,
+            0,
+            (LONG)topSurface_width,
+            (LONG)topSurface_height,
+        };
+        pReal->ColorFill(topSurface, &rect, D3DCOLOR_XRGB(0, 0, 0));
+    }
     
     D3DLOCKED_RECT rect;
     HRESULT hr;
@@ -750,11 +760,27 @@ void SurfaceHook(IDirect3DDevice9 *pReal) {
         (LONG)(rectLeft + w),
         (LONG)(rectTop + h),
     };
+
     if (GRAPHICS_FS_ORIENTATION_SWAP) {
         originRect.left = (topSurface_width / 2) - (GRAPHICS_FS_ORIGINAL_WIDTH / 2);
         originRect.top = (topSurface_height / 2) - (GRAPHICS_FS_ORIGINAL_HEIGHT / 2);
         originRect.right = originRect.left + GRAPHICS_FS_ORIGINAL_WIDTH;
         originRect.bottom = originRect.top + GRAPHICS_FS_ORIGINAL_HEIGHT;
+    }
+
+    bool duplicate = false;
+    RECT originRect2 = originRect;
+    if (cfg::SCREENRESIZE->enable_screen_resize) {
+        auto& scene = cfg::SCREENRESIZE->scene_settings[cfg::SCREENRESIZE->screen_resize_current_scene];
+        if (scene.duplicate == cfg::ScreenDuplicateMode::CopyLeft) {
+            duplicate = true;
+            originRect2.right = originRect.left;
+            originRect2.left = (LONG)(originRect2.left - (originRect.right - originRect.left));
+        } else if (scene.duplicate == cfg::ScreenDuplicateMode::CopyRight) {
+            duplicate = true;
+            originRect2.left = originRect.right;
+            originRect2.right = (LONG)(originRect2.left + (originRect.right - originRect.left));
+        }
     }
 
     // log_misc(
@@ -768,8 +794,20 @@ void SurfaceHook(IDirect3DDevice9 *pReal) {
     if (hr != D3D_OK) {
         log_warning(
             "graphics::d3d9",
-            "SurfaceHook - StretchRect backbuffer failed, rect: {} {} {} {}",
+            "SurfaceHook - StretchRect backbuffer failed for main image, rect: {} {} {} {}",
             originRect.left, originRect.top, originRect.right, originRect.bottom);
+    }
+    if (duplicate) {
+        hr = pReal->StretchRect(
+                backbuffer, nullptr,
+                topSurface, &originRect2,
+                D3DTEXF_LINEAR);
+        if (hr != D3D_OK) {
+            log_warning(
+                "graphics::d3d9",
+                "SurfaceHook - StretchRect backbuffer failed for the duplicate, rect: {} {} {} {}",
+                originRect2.left, originRect2.top, originRect2.right, originRect2.bottom);
+        }
     }
     topSurface->UnlockRect();
 

src/spice2x/overlay/windows/screen_resize.cpp
@@ -55,6 +55,7 @@ namespace overlay::windows {
             scene.offset_y = 0;
             scene.scale_x = 1.f;
             scene.scale_y = 1.f;
+            scene.duplicate = cfg::ScreenDuplicateMode::None;
         }
 
         cfg::SCREENRESIZE->enable_window_resize = false;
@@ -148,6 +149,17 @@ namespace overlay::windows {
             ImGui::SameLine();
             ImGui::HelpMarker("Hint: ctrl + click on the slider to type in a numeric value.");
         }
+
+        static const char* dupe_items[] = { "None", "Copy Left", "Copy Right" };
+        ImGui::Combo(
+            "Duplicate",
+            reinterpret_cast<int *>(&scene.duplicate),
+            dupe_items,
+            ARRAYSIZE(dupe_items));
+        ImGui::SameLine();
+        ImGui::HelpMarker(
+            "Show an identical copy of the image on the left or right, allowing you to achieve "
+            "wrap-around effect when an offset is set.");
 
         ImGui::EndDisabled();
     }

commit 0078b72b03f1a959306db7208728163e526594bf

graphics: address performance regression with Image Resize (#504)
## Link to GitHub Issue, if one exists
Regressed by #401 

## Description of change
Two things from testing SDVX Live2D -

1. ColorFill is almost always too expensive and will lead to drops
1. Scale factor of 0.5 causes GPU to sync and causes drops

(ARGB/XRGB doesn't matter, linear filtering doesn't matter)

1 is fixed by removing ColorFill calls on every frame - instead only do
this once when the user changes the duplicate option (on the next frame)

2 is fixed by updating the UI (limit the slider) though people can
ctrl+click to override these slider limits.

The warning label will be kept with slightly reworded text.

## Testing
tested in sdvx eg final (sudden death) - on my RTX 4070, my FPS is rock
solid 120 fps even with image resize on now.

src/spice2x/cfg/screen_resize.cpp
@@ -81,18 +81,7 @@ namespace cfg {
         
         load_bool_value(doc, root + "enable_screen_resize", this->enable_screen_resize);
         if (this->enable_screen_resize) {
-            if (avs::game::is_model("KFC")) {
-                log_warning(
-                    "ScreenResize",
-                    "Image Resize feature enabled for SDVX; enabling this is known to significantly "
-                    "lower framerate for songs with Live2D!");
-                deferredlogs::defer_error_messages({
-                    "Image Resize + SDVX Live2D performance warning",
-                    "    Enabling Image Resize feature is known to have significant impact",
-                    "    on Live2D performance; you should disable it"});
-            } else {
-                log_misc("ScreenResize", "enabled by config file");
-            }
+            log_misc("ScreenResize", "enabled by config file");
         }
 
         load_bool_value(doc, root + "enable_linear_filter", this->enable_linear_filter);

src/spice2x/cfg/screen_resize.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <atomic>
 #include <memory>
 #include <string>
 #include <optional>
@@ -50,6 +51,9 @@ namespace cfg {
         int8_t screen_resize_current_scene = 0;
         bool enable_linear_filter = true;
         fullscreen_setting scene_settings[4];
+
+        // state
+        std::atomic<bool> need_surface_clean = false;
 
         // windowed mode sizing
         // Windows terminology:

src/spice2x/hooks/graphics/backends/d3d9/d3d9_device.cpp
@@ -742,14 +742,12 @@ void SurfaceHook(IDirect3DDevice9 *pReal) {
     const int w = param.BackBufferWidth;
     const int h = param.BackBufferHeight;
 
-    if (cfg::SCREENRESIZE->enable_screen_resize) {
-        RECT rect {
-            0,
-            0,
-            (LONG)topSurface_width,
-            (LONG)topSurface_height,
-        };
-        pReal->ColorFill(topSurface, &rect, D3DCOLOR_XRGB(0, 0, 0));
+    // this code used to clear the surface using ColorFill on every call, but
+    // this turned out to be very expensive, leading to major frame drops in
+    // SDVX Live2D scenario
+    if (cfg::SCREENRESIZE->need_surface_clean) {
+        pReal->ColorFill(topSurface, nullptr, D3DCOLOR_XRGB(0, 0, 0));
+        cfg::SCREENRESIZE->need_surface_clean = false;
     }
     
     D3DLOCKED_RECT rect;

src/spice2x/overlay/windows/screen_resize.cpp
@@ -103,12 +103,16 @@ namespace overlay::windows {
 
         if (avs::game::is_model("KFC")) {
             ImGui::TextColored(ImVec4(1, 0.5f, 0.5f, 1.f),
-                "Warning: Enabling Image Resize will significantly\n"
-                "lower framerate for songs with Live2D!");
+                "Warning: Enabling Image Resize uses more GPU\n"
+                "resources and may significantly lower framerate\n"
+                "for songs with Live2D! Results may vary, use at\n"
+                "your own risk.");
         } else {
             ImGui::TextColored(ImVec4(1, 0.5f, 0.5f, 1.f),
-                "Warning: Some games are known to have significant\n"
-                "performance issues when Image Resize is enabled.");
+                "Warning: Enabling Image Resize uses more GPU\n"
+                "resources and may significantly lower framerate\n"
+                "in some situations! Results may vary, use at\n"
+                "your own risk.");
         }
 
         // enable checkbox
@@ -152,22 +156,27 @@ namespace overlay::windows {
         // aspect ratio
         ImGui::Checkbox("Keep Aspect Ratio", &scene.keep_aspect_ratio);
         if (scene.keep_aspect_ratio) {
-            if (ImGui::SliderFloat("Scale", &scene.scale_x, 0.5f, 2.5f)) {
+
+            // we want to avoid zooming out below 0.6 as it will cause StretchRect calls to
+            // go on a slow sync path with the GPU, leading to frame drops in some situations
+            // (e.g., SDVX Live2D)
+
+            if (ImGui::SliderFloat("Scale", &scene.scale_x, 0.6f, 2.5f)) {
                 scene.scale_y = scene.scale_x;
             }
             ImGui::SameLine();
             ImGui::HelpMarker("Hint: ctrl + click on the slider to type in a numeric value.");
         } else {
-            ImGui::SliderFloat("Width Scale", &scene.scale_x, 0.5f, 2.5f);
+            ImGui::SliderFloat("Width Scale", &scene.scale_x, 0.6f, 2.5f);
             ImGui::SameLine();
             ImGui::HelpMarker("Hint: ctrl + click on the slider to type in a numeric value.");
-            ImGui::SliderFloat("Height Scale", &scene.scale_y, 0.5f, 2.5f);
+            ImGui::SliderFloat("Height Scale", &scene.scale_y, 0.6f, 2.5f);
             ImGui::SameLine();
             ImGui::HelpMarker("Hint: ctrl + click on the slider to type in a numeric value.");
         }
 
         static const char* dupe_items[] = { "None", "Copy Left", "Copy Right" };
-        ImGui::Combo(
+        const bool duplicate_changed = ImGui::Combo(
             "Duplicate",
             reinterpret_cast<int *>(&scene.duplicate),
             dupe_items,
@@ -178,6 +187,11 @@ namespace overlay::windows {
             "wrap-around effect when an offset is set.");
 
         ImGui::EndDisabled();
+
+        // tell d3d9 device to clear surface to get rid of ghost image
+        if (duplicate_changed) {
+            cfg::SCREENRESIZE->need_surface_clean = true;
+        }
     }
 
     void ScreenResize::build_windowed_config() {