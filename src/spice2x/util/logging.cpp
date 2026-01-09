9c3e4211bcb25f6107be1a851f506907519ea0b8
bicarus-dev <202771338+bicarus-dev@users.noreply.github.com>
2026-01-08 18:37:03 -0800

    clean up time

--- src/spice2x/util/logging.cpp
+++ src/spice2x/util/logging.cpp
@@ -13,8 +13,9 @@ std::string_view log_get_datetime() {
 std::string_view log_get_datetime(std::time_t now) {
     static thread_local char buf[64];
 
-    // `localtime` on Windows is thread-safe
-    strftime(buf, sizeof(buf), "[%Y/%m/%d %X]", localtime(&now));
+    std::tm local_tm{};
+    localtime_s(&local_tm, &now);
+    strftime(buf, sizeof(buf), "[%Y/%m/%d %X]", &local_tm);
 
     return buf;
 }
