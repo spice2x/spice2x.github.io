d2d76b129ef7377d40e5d0ad1900cede42421b20
bicarus-dev <202771338+bicarus-dev@users.noreply.github.com>
2026-07-19 23:42:27 -0700

    fix xp build

--- src/spice2x/misc/nativetouch_inject.h
+++ src/spice2x/misc/nativetouch_inject.h
@@ -2,10 +2,12 @@
 
 #include <windows.h>
 
+struct tagTOUCHINPUT;
+
 namespace nativetouch_inject {
     void attach_window(HWND window);
     void register_and_attach_window(HWND window);
     void hook(HMODULE module);
     bool inject_external_touch(POINT position, bool down);
-    void transform_touch_input(PTOUCHINPUT point);
+    void transform_touch_input(tagTOUCHINPUT *point);
 }
