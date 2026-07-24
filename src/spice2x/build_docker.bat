e4d1bab96868cc88233dfb0fa9902a8be293109b
bicarus-dev <202771338+bicarus-dev@users.noreply.github.com>
2026-07-23 21:27:48 -0700

    add version

--- src/spice2x/build_docker.bat
+++ src/spice2x/build_docker.bat
@@ -1,7 +1,15 @@
+setlocal
+pushd "%~dp0" || exit /b 1
+
 docker build --pull external/docker -t spicetools/deps
 docker build --build-context gitroot=%cd%/../../.git . -t spicetools/spice
 docker run --rm -i -v %cd%/dist:/src/src/spice2x/dist -v %cd%/bin:/src/src/spice2x/bin -v %cd%/.ccache:/src/src/spice2x/.ccache spicetools/spice %*
+set "exit_code=%errorlevel%"
+
 @REM to generate PDBs, set DEBUG to 1 in build_all.sh, place cv2pdb in external\cv2pdb, and run below
 @REM external\cv2pdb\cv2pdb.exe bin\spice2x\spicecfg.exe bin\spice2x\spicecfg-pdb.exe bin\spice2x\spicecfg-pdb.pdb
 @REM external\cv2pdb\cv2pdb.exe bin\spice2x\spice.exe bin\spice2x\spice-pdb.exe bin\spice2x\spice-pdb.pdb
 @REM external\cv2pdb\cv2pdb.exe bin\spice2x\spice64.exe bin\spice2x\spice64-pdb.exe bin\spice2x\spice64-pdb.pdb
+
+popd
+exit /b %exit_code%
--- src/spice2x/build_release.bat
+++ src/spice2x/build_release.bat
@@ -1,3 +1,10 @@
+setlocal
+pushd "%~dp0" || exit /b 1
+
 del /s /q .ccache
 del /s /q dist
 call build_docker.bat
+
+set "exit_code=%errorlevel%"
+popd
+exit /b %exit_code%
