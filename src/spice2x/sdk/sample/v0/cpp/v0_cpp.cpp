6f3f4502dd08ecac434444e237a773f45f9eb6a4
bicarus-dev <202771338+bicarus-dev@users.noreply.github.com>
2026-09-05 03:48:33 -0700

    update sample

--- src/spice2x/sdk/sample/v0/cpp/v0_cpp.cpp
+++ src/spice2x/sdk/sample/v0/cpp/v0_cpp.cpp
@@ -76,20 +76,32 @@ static ArrowButton arrow_buttons[] = {
 
 // worker thread for I/O
 static void worker_thread_main(std::stop_token stop_token) {
-    bool coin_previous_state = false;
+    bool coin_previous_state[10] = {};
     while (!stop_token.stop_requested()) {
 
         // insert coin
-        const bool coin_pressed = (GetAsyncKeyState(VK_INSERT) & 0x8000) != 0;
-        if (coin_pressed && !coin_previous_state && spice.insert_coin) {
-            const auto status = spice.insert_coin(1);
-            if (status != SPICE_SDK_STATUS_SUCCESS) {
-                LOG_INFO(std::format(
-                    "coin insertion failed: {}",
-                    static_cast<int>(status)).c_str());
+        const bool control_pressed = (GetAsyncKeyState(VK_CONTROL) & 0x8000) != 0;
+        for (uint8_t amount = 0; amount < 10; amount++) {
+            const bool coin_pressed = control_pressed &&
+                ((GetAsyncKeyState('0' + amount) & 0x8000) != 0);
+
+            if (coin_pressed && !coin_previous_state[amount] && spice.insert_coin) {
+                const auto status = spice.insert_coin(amount);
+                if (status != SPICE_SDK_STATUS_SUCCESS) {
+                    LOG_INFO(std::format(
+                        "coin insertion failed: {}",
+                        static_cast<int>(status)).c_str());
+                } else {
+                    const auto message = std::format(
+                        "v0_cpp: inserted {} coin{}", amount, amount == 1 ? "" : "s");
+                    LOG_INFO(message.c_str());
+                    if (spice.add_toast) {
+                        spice.add_toast(SPICE_SDK_TOAST_LEVEL_SUCCESS, message.c_str());
+                    }
+                }
             }
+            coin_previous_state[amount] = coin_pressed;
         }
-        coin_previous_state = coin_pressed;
 
         // check for ctrl + arrow keys and trigger p1 pad arrows
         for (auto& arrow : arrow_buttons) {
@@ -102,7 +114,7 @@ static void worker_thread_main(std::stop_token stop_token) {
                     if (spice.add_toast) {
                         spice.add_toast(
                             SPICE_SDK_TOAST_LEVEL_INFO,
-                            std::format("let me hear you say: {}", arrow.name).c_str());
+                            std::format("v0_cpp: let me hear you say: {}", arrow.name).c_str());
                     } else {
                         LOG_INFO(std::format("let me hear you say: {}", arrow.name).c_str());
                     }
